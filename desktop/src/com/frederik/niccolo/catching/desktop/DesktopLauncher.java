638f26b50c375105d5819efead06d963c0ebb361
Niccolo <niccomaz04@gmail.com>
2019-04-12 16:43:12 +0200

    Libgdx(Grundlage)

---
+++ desktop/src/com/frederik/niccolo/catching/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frederik.niccolo.catching.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frederik.niccolo.catching.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
