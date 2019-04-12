fc20edb58cadbf044e61ee765438dd2056045ed9
Kodas04 <niccomaz04@gmail.com>
2019-04-12 17:25:08 +0200

    Ich habe angefangen die Mainklasse zu programmieren zusammen mit dem ersten Screen

--- core/src/com/frederik/niccolo/catching/Main.java
+++ core/src/com/frederik/niccolo/catching/Main.java
@@ -1,33 +1,47 @@
 package com.frederik.niccolo.catching;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.frederik.niccolo.catching.screens.*;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
+	
 	SpriteBatch batch;
 	Texture img;
 	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		setScreen(new Game());
+		
 	}
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		
+	}
+	@Override
+	public void render() {
+		super.render();
+		
+	}
+	@Override
+	public void pause() {
+		super.pause();
+		
+	}
+	@Override
+	public void resume() {
+		super.pause();
+		
+	}
+	@Override
+	public void dispose() {
+		super.dispose();
+		
+	}
+	
+
 }
